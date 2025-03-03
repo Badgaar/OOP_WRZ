7fc63c474c2c5128887572d1d297ce25fa63c4d1
SimpleBadger <wojciechrzucidlo22@gmail.com>
2025-03-03 14:57:34 +0100

    Initial Commit

--- src/main/java/baadgar/roguelike/Main.java
+++ src/main/java/baadgar/roguelike/Main.java
@@ -1,7 +1,12 @@
 package baadgar.roguelike;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s);
     }
 }
